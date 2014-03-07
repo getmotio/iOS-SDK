67263a7483f091aed2c87a09520505c027e3a001
Paul Ballesty <paulballesty@gmail.com>
2014-03-06 21:51:59 -0300

    Initial Commit

---
+++ Example/Example/Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Paul Ballesty on 3/6/14.
+//  Copyright (c) 2014 Motio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MOAppDelegate class]));
+    }
+}
