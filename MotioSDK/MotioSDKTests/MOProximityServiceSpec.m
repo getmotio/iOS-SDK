57c650ba1f10dc7620362839178bf8fc33a3305a
Paul Ballesty <paulballesty@gmail.com>
2014-03-09 16:04:02 -0300

    Adding 1 test to MOProximityServiceSpec

--- MotioSDK/MotioSDKTests/MOProximityServiceSpec.m
+++ MotioSDK/MotioSDKTests/MOProximityServiceSpec.m
@@ -7,8 +7,28 @@
 //
 
 #import "Kiwi.h"
+#import <MotioSDK/MOProximityService.h>
+
+@interface MOProximityService()
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CLBeaconRegion *regionOfInterest;
+@end
 
 SPEC_BEGIN(MOProximityServiceSpec)
 
+describe(NSStringFromClass(MOProximityService.class), ^{
+    
+    __block NSString *uuidString = @"D57092AC-DFAA-446C-8EF3-C81AA22815B5";
+    __block MOProximityService *proximityService;
+    
+    beforeEach(^{
+        proximityService = [[MOProximityService alloc] initWithRegion:@{MOProximityServiceUUIDKey: uuidString}];
+    });
+    
+    it(@"should initialize the region of interest", ^{
+        [[[[proximityService.regionOfInterest proximityUUID] UUIDString] should] equal:uuidString];
+    });
+    
+});
 
 SPEC_END
